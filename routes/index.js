ea9062e155aa2dbe7bea490481822ed7ee23dcf8
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-10-02 12:06:44 +0300

    new

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-// import UsersController from '../controllers/UsersController';
+import UsersController from '../controllers/UsersController';
 // import AuthController from '../controllers/AuthController';
 // import FilesController from '../controllers/FilesController';
 
@@ -10,13 +10,13 @@ router.get('/status', AppController.getStatus);
 
 router.get('/stats', AppController.getStats);
 
-// router.post('/users', UsersController.postNew);
+router.post('/users', UsersController.postNew);
 
 // router.get('/connect', AuthController.getConnect);
 
 // router.get('/disconnect', AuthController.getDisconnect);
 
-// router.get('/users/me', UsersController.getMe);
+router.get('/users/me', UsersController.getMe);
 
 // router.post('/files', FilesController.postUpload);
 
