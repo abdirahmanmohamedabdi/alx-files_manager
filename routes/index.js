4ef602961760ee3f03cb7d38016d28e3d3e28537
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-10-02 13:21:22 +0300

    new

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-// import FilesController from '../controllers/FilesController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -18,16 +18,16 @@ router.get('/disconnect', AuthController.getDisconnect);
 
 router.get('/users/me', UsersController.getMe);
 
-// router.post('/files', FilesController.postUpload);
+router.post('/files', FilesController.postUpload);
 
-// router.get('/files/:id', FilesController.getShow);
+router.get('/files/:id', FilesController.getShow);
 
-// router.get('/files', FilesController.getIndex);
+router.get('/files', FilesController.getIndex);
 
-// router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putPublish);
 
-// router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
-// router.get('/files/:id/data', FilesController.getFile);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
